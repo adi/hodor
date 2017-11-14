ebe522253692894624920e4522ae6da6e964c5b6
Adrian Punga <adrian.punga@sqreept.com>
2017-11-14 13:28:38 +0200

    Partial

--- legend.js
+++ legend.js
@@ -1,20 +1,20 @@
 var legend = [
   // standard js words
-  { search: 'break', replace: '!HODOR;;;' },
-  { search: 'case', replace: '?hooDooRrr' },
-  { search: 'catch', replace: '!!!HODOR!!!' },
-  { search: 'else', replace: 'hoDOR!!!' },
-  { search: 'for', replace: 'HODOR{}' },
-  { search: 'function', replace: 'HoDoRHoDoR' },
-  { search: 'if', replace: 'HOdor!!!' },
-  { search: 'instanceof', replace: 'hodorhodorho' },
+  { search: 'break', replace: '!EJOBS;;;' },
+  { search: 'case', replace: '?ejjObbSss' },
+  { search: 'catch', replace: '!!!EJOBS!!!' },
+  { search: 'else', replace: 'ejOBS!!!' },
+  { search: 'for', replace: 'EJOBS{}' },
+  { search: 'function', replace: 'EjObSEjObS' },
+  { search: 'if', replace: 'EJobs!!!' },
+  { search: 'instanceof', replace: 'ejobsejobsej' },
   { search: 'new', replace: 'HHHOOODDDDOOORRR!!!' },
-  { search: 'return', replace: 'HODOR::' },
-  { search: 'switch', replace: 'HODORHODOR' },
+  { search: 'return', replace: 'EJOBS::' },
+  { search: 'switch', replace: 'EJOBSEJOBS' },
   { search: 'throw', replace: '!hOdor!' },
   { search: 'try', replace: '!hodOr!' },
   { search: 'typeof', replace: 'Hodor?????????!?!?!?!?' },
-  { search: 'var', replace: '$HODOR:' },
+  { search: 'var', replace: '$EJOBS:' },
   { search: 'while', replace: 'hodor............' },
   { search: 'console.log', replace: 'hodor.hod' },
 
@@ -22,77 +22,77 @@ var legend = [
   // s a c m p t b f g i n e l w u v j k q y z x
   // "x "
   { search: 's', replace: 'Hodor ' },
-  { search: 'a', replace: 'HODOR ' },
+  { search: 'a', replace: 'EJOBS ' },
   { search: 'c', replace: 'hodor ' },
   
   // "x. "
   { search: 'm', replace: 'Hodor. ' },
-  { search: 'p', replace: 'HODOR. ' },
+  { search: 'p', replace: 'EJOBS. ' },
   { search: 't', replace: 'hodor. ' },
 
   // "x! "
   { search: 'b', replace: 'Hodor! ' },
-  { search: 'f', replace: 'HODOR! ' },
+  { search: 'f', replace: 'EJOBS! ' },
   { search: 'g', replace: 'hodor! ' },
 
   // "x? "
   { search: 'i', replace: 'Hodor? ' },
-  { search: 'n', replace: 'HODOR? ' },
+  { search: 'n', replace: 'EJOBS? ' },
   { search: 'e', replace: 'hodor? ' },
 
   // "x!? "
   { search: 'l', replace: 'Hodor!? ' },
-  { search: 'w', replace: 'HODOR!? ' },
+  { search: 'w', replace: 'EJOBS!? ' },
   { search: 'u', replace: 'hodor!? ' },
 
   // "x?! "
   { search: 'v', replace: 'Hodor?! ' },
-  { search: 'j', replace: 'HODOR?! ' },
+  { search: 'j', replace: 'EJOBS?! ' },
   { search: 'k', replace: 'hodor?! ' },
 
   // "x!?! "
   { search: 'q', replace: 'Hodor!?! ' },
-  { search: 'y', replace: 'HODOR!?! ' },
+  { search: 'y', replace: 'EJOBS!?! ' },
   { search: 'z', replace: 'hodor!?! ' },
 
   // "x?!? "
   { search: 'x', replace: 'Hodor?!? ' },
-  { search: 'S', replace: 'HODOR?!? ' },
+  { search: 'S', replace: 'EJOBS?!? ' },
   { search: 'A', replace: 'hodor?!? ' },
 
   // "x... "
   { search: 'C', replace: 'Hodor... ' },
-  { search: 'M', replace: 'HODOR... ' },
+  { search: 'M', replace: 'EJOBS... ' },
   { search: 'P', replace: 'hodor... ' },
 
   // "x-"
   { search: 'T', replace: 'Hodor- ' },
-  { search: 'B', replace: 'HODOR- ' },
+  { search: 'B', replace: 'EJOBS- ' },
   { search: 'F', replace: 'hodor- ' },
 
   // "prepending HoOodoOor"
   { search: 'G', replace: 'HoOodoOorHodor ' },
-  { search: 'I', replace: 'HoOodoOorHODOR ' },
+  { search: 'I', replace: 'HoOodoOorEJOBS ' },
   { search: 'N', replace: 'HoOodoOorhodor ' },
 
   // "prepending Hooodorr"
   { search: 'E', replace: 'HooodorrHodor ' },
-  { search: 'L', replace: 'HooodorrHODOR ' },
+  { search: 'L', replace: 'HooodorrEJOBS ' },
   { search: 'W', replace: 'Hooodorrhodor ' },
 
   // "appending Hooodorr"
   { search: 'U', replace: 'HodorHooodorr ' },
-  { search: 'V', replace: 'HODORHooodorr ' },
+  { search: 'V', replace: 'EJOBSHooodorr ' },
   { search: 'J', replace: 'hodorHooodorr ' },
 
   // "appending HoOodoOor"
   { search: 'K', replace: 'HodorHoOodoOor ' },
-  { search: 'Q', replace: 'HODORHoOodoOor ' },
+  { search: 'Q', replace: 'EJOBSHoOodoOor ' },
   { search: 'Y', replace: 'hodorHoOodoOor ' },
 
   // "prepending HoOodoOorHooodorr"
   { search: 'Z', replace: 'HoOodoOorHooodorrHodor ' },
-  { search: 'X', replace: 'HoOodoOorHooodorrHODOR ' },
+  { search: 'X', replace: 'HoOodoOorHooodorrEJOBS ' },
   //Add Hungarian unique characters to Hodor language  { search: 'á', replace: 'Hoodor ' },
   { search: 'Á', replace: 'HOodor ' },
   { search: 'é', replace: 'Hodoor ' },
