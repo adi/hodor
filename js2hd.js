e833022883eb210035bed4a93b25437f2c2f51df
Adrian Punga <adrian.punga@sqreept.com>
2017-11-14 13:24:22 +0200

    eJobs

--- js2hd.js
+++ js2hd.js
@@ -13,33 +13,33 @@ var legend = require('./legend'); // ./ means current directory, and don't need 
 
 program
   .version(module.exports.version, '-v, --version')
-  .description('Hodor hodor hodor')
+  .description('eJobs eJobs eJobs')
   .parse(process.argv);
 
-var hodor = program.args[0];
+var eJobs = program.args[0];
 
-if( typeof(hodor) === 'undefined') {
-  console.log('HODOR:'.bold.red + ' hodor hodor hodor!'.red);
+if( typeof(eJobs) === 'undefined') {
+  console.log('EJOBS:'.bold.red + ' eJobs eJobs eJobs!'.red);
 } else {
-  if (hodor.search(".js") > 0) { // user entered a .js file
-    console.log('HODOR: '.bold.cyan + hodor.bold.white + ' => '.yellow + hodor.replace('.js', '.hd').bold.white);
-    var text = fs.readFileSync(hodor).toString(); // the contents of the file
+  if (eJobs.search(".js") > 0) { // user entered a .js file
+    console.log('EJOBS: '.bold.cyan + eJobs.bold.white + ' => '.yellow + eJobs.replace('.js', '.hd').bold.white);
+    var text = fs.readFileSync(eJobs).toString(); // the contents of the file
     convertCode(text);
   } else { // user entered something apart from a js file
-    console.log('HODOR:'.bold.red + ' hodor hodor!'.red);
+    console.log('EJOBS:'.bold.red + ' eJobs eJobs!'.red);
   }
 }
 
 function convertCode (text) {
-  var outputFileName = hodor.replace(".js", ".hd");
-  var hodorText = text;
+  var outputFileName = eJobs.replace(".js", ".hd");
+  var eJobsText = text;
   
   for (i = 0; i < legend.length; i++){
     var query = legend[i];
     var re    = new RegExp(query.search, 'g');
 
-    hodorText = hodorText.replace(re, query.replace);
+    eJobsText = eJobsText.replace(re, query.replace);
   }
   
-  fs.writeFileSync(outputFileName, hodorText);  
+  fs.writeFileSync(outputFileName, eJobsText);  
 }
